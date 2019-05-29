515e973af810161a79799cfcaecccb46646ce349
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-29 10:03:25 +0100

    Configure setup function as a global

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
 import Slider from './Slider';
 
-function setup() {
-  const props = {};
-  const wrapper = shallow(<App {...props} />);
-
-  return { props, wrapper };
-}
+const props = {};
 
 describe('App renders', () => {
   test('a Slider', () => {
-    const { wrapper } = setup();
-    expect(wrapper.find(Slider).length).toEqual(1);
+    const { shallowWrapper } = setup(App, props);
+    expect(shallowWrapper.find(Slider).length).toEqual(1);
   });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,33 @@
-import { configure } from 'enzyme';
+import { createElement } from 'react';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+function setup(component, props, renderMethod = 'shallow') {
+  let shallowWrapper,
+    mountWrapper,
+    wrappers = {};
+  const element = createElement(component, props);
+
+  switch (renderMethod) {
+    case 'all':
+      shallowWrapper = shallow(element);
+      mountWrapper = mount(element);
+      wrappers = { ...wrappers, mountWrapper, shallowWrapper };
+      break;
+
+    case 'mount':
+      mountWrapper = mount(element);
+      wrappers = { ...wrappers, mountWrapper };
+      break;
+
+    default:
+      shallowWrapper = shallow(element);
+      wrappers = { ...wrappers, shallowWrapper };
+  }
+
+  return { ...wrappers, props };
+}
+
+global.setup = setup;
