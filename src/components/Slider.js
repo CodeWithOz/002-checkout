bfbec2c466f2e49b5e72cb137f3d2c438008d0f7
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-02 13:49:00 +0100

    Supply correct name to Slider's Field

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+const sliderConfig = {
+  field: {
+    name: 'range'
+  }
+};
+
 export function Slider() {
+  const { field } = sliderConfig;
+
   return (
     <form>
-      <Field name="test" component={() => <div>Test</div>} />
+      <Field {...field} component={() => <div>Test</div>} />
     </form>
   );
 }
