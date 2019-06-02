c5e9fb1913a3db97e6de1482ea70a1176dec2ece
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-02 15:57:13 +0100

    Test if SliderRange passes its type prop

--- src/components/formFields/SliderRange.test.js
+++ src/components/formFields/SliderRange.test.js
@@ -1,10 +1,19 @@
 import SliderRange from './SliderRange';
 
-const props = {};
+const props = {
+  type: 'test'
+};
 
 describe('SliderRange renders', () => {
   test('an input element', () => {
     const { shallowWrapper } = setup(SliderRange, props);
     expect(shallowWrapper.find('input').length).toEqual(1);
+  });
+});
+
+describe('SliderRange correctly passes', () => {
+  test(`its 'type' prop to its input element`, () => {
+    const { shallowWrapper } = setup(SliderRange, props);
+    expect(shallowWrapper.find('input').prop('type')).toEqual(props.type);
   });
 });
