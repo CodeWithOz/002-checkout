0dd90598d3e880b1362cd0d6c4d4988eda26999c
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-02 07:43:03 +0100

    Test if Slider renders a redux-form Field

--- src/components/Slider.test.js
+++ src/components/Slider.test.js
@@ -1,3 +1,4 @@
+import { Field } from 'redux-form';
 import Slider from './Slider';
 
 const props = {};
@@ -6,5 +7,10 @@ describe('Slider renders', () => {
   test('a form', () => {
     const { shallowWrapper } = setup(Slider, props);
     expect(shallowWrapper.find('form').length).toEqual(1);
+  });
+
+  test('a Field', () => {
+    const { shallowWrapper } = setup(Slider, props);
+    expect(shallowWrapper.find(Field).length).toEqual(1);
   });
 });
