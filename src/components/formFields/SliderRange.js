616bb0a62d0d23b531b3ebff48b3bd7b9aff27a4
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-02 16:09:22 +0100

    Add prop types for SliderRange

--- src/components/formFields/SliderRange.js
+++ src/components/formFields/SliderRange.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function SliderRange({ type }) {
   return <input type={type} />;
 }
+
+SliderRange.propTypes = {
+  type: PropTypes.string.isRequired
+};
