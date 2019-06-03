9034ab150e8b26c800da28529217581e8563556a
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-03 18:02:44 +0100

    Test if App renders a ProductDisplay component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,6 @@
 import App from './App';
 import Slider from './Slider';
+import ProductDisplay from './ProductDisplay';
 
 const props = {};
 
@@ -7,5 +8,10 @@ describe('App renders', () => {
   test('a Slider', () => {
     const { shallowWrapper } = setup(App, props);
     expect(shallowWrapper.find(Slider).length).toEqual(1);
+  });
+
+  test('a ProductDisplay', () => {
+    const { shallowWrapper } = setup(App, props);
+    expect(shallowWrapper.find(ProductDisplay).length).toEqual(1);
   });
 });
