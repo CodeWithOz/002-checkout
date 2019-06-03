a7e7009be4d8626eedef8ff209629f40aa3a9c24
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-03 18:16:52 +0100

    Test if BgImgContainer renders its children

--- src/components/BgImgContainer.test.js
+++ src/components/BgImgContainer.test.js
@@ -1,10 +1,17 @@
 import BgImgContainer from './BgImgContainer';
 
-const props = {};
+const props = {
+  children: 'test'
+};
 
 describe('BgImgContainer renders', () => {
   test('a div', () => {
     const { shallowWrapper } = setup(BgImgContainer, props);
     expect(shallowWrapper.find('div').length).toEqual(1);
+  });
+
+  test(`its 'children' prop`, () => {
+    const { shallowWrapper } = setup(BgImgContainer, props);
+    expect(shallowWrapper.text()).toContain(props.children);
   });
 });
