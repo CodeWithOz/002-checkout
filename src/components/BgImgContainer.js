2d975fcd3f8cce0a2cf955270f60fed2163c69d5
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-04 05:40:40 +0100

    Wrap bgImgUrl with url() function
    Width and height have been set inline to ensure
    that the background image shows. They will
    eventually be set with external CSS.

--- src/components/BgImgContainer.js
+++ src/components/BgImgContainer.js
@@ -2,7 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function BgImgContainer({ bgImgUrl, children }) {
-  return <div style={{ backgroundImage: bgImgUrl }}>{children}</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${bgImgUrl})`,
+        width: '800px',
+        height: '800px'
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 BgImgContainer.propTypes = {
