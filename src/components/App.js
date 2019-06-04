e58ccc75c694868f1d21ed2fc5a4f3ddda6ff34d
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-04 05:37:55 +0100

    Supply a value for bgImgUrl

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Slider from './Slider';
 import BgImgContainer from './BgImgContainer';
 
+const appConfig = {
+  bgImgUrl: 'https://source.unsplash.com/900x900/?office'
+};
+
 export default function App() {
   return (
     <div>
-      <BgImgContainer />
+      <BgImgContainer bgImgUrl={appConfig.bgImgUrl} />
       <Slider />
     </div>
   );
