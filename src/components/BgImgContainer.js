9bb4fc2fa5d3e7b71dbc93061b54958a20cd87f9
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-04 05:21:54 +0100

    Pass bgImgUrl prop as backgroundImage
    App doesn't yet supply this prop to BgImgContainer so
    there is no background image showing in the app for now.

--- src/components/BgImgContainer.js
+++ src/components/BgImgContainer.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function BgImgContainer({ children }) {
-  return <div>{children}</div>;
+export default function BgImgContainer({ bgImgUrl, children }) {
+  return <div style={{ backgroundImage: bgImgUrl }}>{children}</div>;
 }
+
+BgImgContainer.propTypes = {
+  bgImgUrl: PropTypes.string.isRequired
+};
