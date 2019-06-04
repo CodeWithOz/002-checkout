2fd81ce52b98b0647d68745967e7dd5ef47fdc7b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-04 11:35:43 +0100

    Add dimensions to make background image show up
    Dimensions were moved from inline style to external stylesheet.

--- src/components/BgImgContainer/BgImgContainer.js
+++ src/components/BgImgContainer/BgImgContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './BgImgContainer.module.css';
 
 export default function BgImgContainer({ bgImgUrl, children }) {
   return (
     <div
       style={{
-        backgroundImage: `url(${bgImgUrl})`,
-        width: '800px',
-        height: '800px'
+        backgroundImage: `url(${bgImgUrl})`
       }}
+      className={styles.BgImgContainer}
     >
       {children}
     </div>
