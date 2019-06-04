16002ad564b536735f36e86715363f4c8d52a1c4
Uche Ozoemena <ucheozoemena@gmail.com>
2019-06-04 12:10:35 +0100

    Change bgImgUrl

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BgImgContainer, ProductDisplay, Slider } from '../../components';
 
 const appConfig = {
-  bgImgUrl: 'https://source.unsplash.com/900x900/?office'
+  bgImgUrl: 'https://source.unsplash.com/1600x900/?office,room'
 };
 
 export default function App() {
