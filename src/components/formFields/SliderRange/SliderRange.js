12055c92ef3231568076bbaf33a18a48e02857fc
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-15 18:09:22 +0100

    Pass 'max' prop to input field

--- src/components/formFields/SliderRange/SliderRange.js
+++ src/components/formFields/SliderRange/SliderRange.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function SliderRange({ type, input }) {
-  return <input type={type} {...input} />;
+export default function SliderRange({ input, meta, ...restProps }) {
+  return <input {...restProps} {...input} />;
 }
 
 SliderRange.propTypes = {
