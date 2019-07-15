faa886abc0e4a61a544ab148cf0507d4019d8a84
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-15 19:38:56 +0100

    Render the bg image supplied to ProductDisplay

--- src/components/ProductDisplay/ProductDisplay.js
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { string } from 'prop-types';
 
-export default function ProductDisplay() {
-  return <div>ProductDisplay</div>;
+export default function ProductDisplay({ bgImgUrl }) {
+  return (
+    <div style={{ backgroundImage: `url(${bgImgUrl})` }}>ProductDisplay</div>
+  );
 }
+
+ProductDisplay.propTypes = {
+  bgImgUrl: string
+};
