c9ffc82a23b978feff007bdf08800783b9fdc822
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 12:44:15 +0100

    Calculate and render breakdown items

--- src/components/Breakdown/Breakdown.js
+++ src/components/Breakdown/Breakdown.js
@@ -17,12 +17,47 @@ export const BreakdownItem = ({ label, value }) => (
   </div>
 );
 
-const Breakdown = () => {
+const Breakdown = ({ days, price, discount, tax }) => {
+  const breakdownItems = [];
+  const daysLabel = `${price} GBP x ${days} days`;
+  const daysPrice = price * days;
+  const daysValue = `${daysPrice} GBP`;
+  breakdownItems.push({ label: daysLabel, value: daysValue });
+
+  const discountLabel = 'Discount';
+  const discountPrice = discount * days;
+  const discountValue = `${discountPrice} GBP`;
+  breakdownItems.push({ label: discountLabel, value: discountValue });
+
+  const subtotalLabel = 'Subtotal';
+  const subtotalPrice = daysPrice - discountPrice;
+  const subtotalValue = `${subtotalPrice} GBP`;
+  breakdownItems.push({ label: subtotalLabel, value: subtotalValue });
+
+  const taxLabel = 'Tax';
+  const taxPrice = tax * days;
+  const taxValue = `${taxPrice} GBP`;
+  breakdownItems.push({ label: taxLabel, value: taxValue });
+
+  const totalLabel = 'TOTAL';
+  const totalPrice = subtotalPrice + taxPrice;
+  const totalValue = `${totalPrice} GBP`;
+
   const { slider } = breakdownConfig;
 
   return (
     <div>
       <Slider {...slider} />
+      <ul>
+        {breakdownItems.map((itemProps, idx) => (
+          <li key={idx}>
+            <BreakdownItem {...itemProps} />
+          </li>
+        ))}
+        <li>
+          <BreakdownItem label={totalLabel} value={totalValue} />
+        </li>
+      </ul>
     </div>
   );
 };
