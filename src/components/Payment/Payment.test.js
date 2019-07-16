800c9298772a790c420e396287675b4b53487b84
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 12:12:32 +0100

    Test if Payment passes discount prop to Breakdown

--- src/components/Payment/Payment.test.js
+++ src/components/Payment/Payment.test.js
@@ -1,11 +1,20 @@
 import Payment from './Payment';
 import { Breakdown } from '../../components';
 
-const props = {};
+const props = { discount: 9 };
 
 describe('Payment renders', () => {
   test('a Breakdown component', () => {
     const { shallowWrapper } = setup(Payment, props);
     expect(shallowWrapper.find(Breakdown).length).toEqual(1);
+  });
+});
+
+describe('Payment correctly passes', () => {
+  test(`its 'discount' prop to Breakdown`, () => {
+    const { shallowWrapper } = setup(Payment, props);
+    expect(shallowWrapper.find(Breakdown).prop('discount')).toEqual(
+      props.discount
+    );
   });
 });
