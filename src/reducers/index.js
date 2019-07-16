3dd3e5f5d3c30628cfbf331d40a2b08164680d87
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 08:23:58 +0100

    Add tax to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,6 @@ import { reducer as formReducer } from 'redux-form';
 export default combineReducers({
   form: formReducer,
   price: () => 320,
-  discount: () => 16
+  discount: () => 16,
+  tax: () => 60
 });
