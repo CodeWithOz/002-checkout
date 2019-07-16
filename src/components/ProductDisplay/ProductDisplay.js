9e5b8068a65d36c8a08f183ca345ca8ee3ba876f
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 05:33:28 +0100

    Add most text of ProductDisplay (no styling yet)

--- src/components/ProductDisplay/ProductDisplay.js
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { string } from 'prop-types';
 
+const productDisplayConfig = {
+  listingTitle: 'Coworking Space, South Korea',
+  numPeople: '1 person',
+  listingDates: {
+    start: 'Mon, Aug 22, 2016',
+    end: 'Fri, Aug 26, 2016'
+  }
+};
+
 export default function ProductDisplay({ bgImgUrl }) {
+  const {
+    listingTitle,
+    numPeople,
+    listingDates: { start, end }
+  } = productDisplayConfig;
+
   return (
-    <div style={{ backgroundImage: `url(${bgImgUrl})` }}>ProductDisplay</div>
+    <div style={{ backgroundImage: `url(${bgImgUrl})` }}>
+      <div>
+        <h2>{listingTitle}</h2>
+        <p>
+          {1 /* price will go here */} GBP <span>/ 1 day</span>
+        </p>
+        <p>
+          Entire office for <span>{numPeople}</span>
+        </p>
+        <p>
+          <span>{start}</span> to <span>{end}</span>
+        </p>
+      </div>
+    </div>
   );
 }
 
