5358d40e02bfdd1d676e53fb68e63c89f3a06909
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 06:37:16 +0100

    Test if Breakdown renders a Slider

--- src/components/Breakdown/Breakdown.test.js
+++ src/components/Breakdown/Breakdown.test.js
@@ -1,10 +1,18 @@
 import Breakdown from './Breakdown';
+import Slider from '../Slider/Slider';
 
 const props = {};
 
 describe('Breakdown renders', () => {
-  test('something', () => {
+  test('a Slider', () => {
     const { shallowWrapper } = setup(Breakdown, props);
-    expect(shallowWrapper.children()).toBeDefined();
+    expect(shallowWrapper.find(Slider).length).toEqual(1);
+  });
+
+  test('a Slider with the initial slider value set to 50', () => {
+    const { shallowWrapper } = setup(Breakdown, props);
+    expect(shallowWrapper.find(Slider).prop('initialValues')).toEqual({
+      range: '50'
+    });
   });
 });
