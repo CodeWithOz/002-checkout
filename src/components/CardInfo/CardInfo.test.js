7722bf9ddac61efbc12741d3db4207bea82cb6ed
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 14:24:07 +0100

    Test if CardInfo renders a form

---
+++ src/components/CardInfo/CardInfo.test.js
@@ -0,0 +1,10 @@
+import CardInfo from './CardInfo';
+
+const props = {};
+
+describe('CardInfo renders', () => {
+  test('a form', () => {
+    const { shallowWrapper } = setup(CardInfo, props);
+    expect(shallowWrapper.find('form').length).toEqual(1);
+  });
+});
