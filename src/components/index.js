2dda7a8b21f0b2f42f52c2b140c764de2db2871d
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 06:47:48 +0100

    Add Payment and Breakdown to exported components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as BgImgContainer } from './BgImgContainer/BgImgContainer';
 export { default as ProductDisplay } from './ProductDisplay/ProductDisplay';
 export { default as SliderRange } from './formFields/SliderRange/SliderRange';
 export { default as Slider } from './Slider/Slider';
+export { default as Payment } from './Payment/Payment';
+export { default as Breakdown } from './Breakdown/Breakdown';
