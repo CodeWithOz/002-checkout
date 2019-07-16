7bb61a62ec1eb9ece9b8978dbdd1da917f95fe2d
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 14:35:28 +0100

    Connect CardInfo's form to redux form

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
 
-export default function CardInfo() {
+export function CardInfo() {
   return (
     <div>
+      <h3>Payment Information</h3>
       <form>CardInfo</form>
     </div>
   );
 }
+
+export default reduxForm({ form: 'CardInfo' })(CardInfo);
