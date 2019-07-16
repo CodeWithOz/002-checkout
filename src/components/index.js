0249ab97438e70fcd42627d34f1b84fc03675cf8
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 18:26:42 +0100

    Add TextField to exported components

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as Slider } from './Slider/Slider';
 export { default as Payment } from './Payment/Payment';
 export { default as Breakdown } from './Breakdown/Breakdown';
 export { default as CardInfo } from './CardInfo/CardInfo';
+export { default as TextField } from './formFields/TextField/TextField';
