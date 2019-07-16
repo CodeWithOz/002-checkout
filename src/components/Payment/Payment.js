12291223aaf71331cbfc55f8f478a20e57fe3a27
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 11:57:21 +0100

    Connect Payment component to redux store

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Breakdown } from '../../components';
 
 const Payment = () => <Breakdown />;
 
-export default Payment;
+const mapStateToProps = ({ price, discount, tax, form }) => {
+  const stateAsProps = { price, discount, tax };
+
+  const formReady =
+    form && form.Slider && form.Slider.values && form.Slider.values.range;
+  if (formReady) {
+    stateAsProps.days = form.Slider.values.range;
+  }
+
+  return stateAsProps;
+};
+
+export default connect(mapStateToProps)(Payment);
