555a49764d370da6e40e86cda6444cf3947a125e
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 18:16:47 +0100

    Test if form renders a 'cc-name' Field

--- src/components/CardInfo/CardInfo.test.js
+++ src/components/CardInfo/CardInfo.test.js
@@ -7,4 +7,12 @@ describe('CardInfo renders', () => {
     const { shallowWrapper } = setup(CardInfo, props);
     expect(shallowWrapper.find('form').length).toEqual(1);
   });
+
+  describe('a Field with', () => {
+    test(`'name' set to 'cc-name'`, () => {
+      const { shallowWrapper } = setup(CardInfo, props);
+      expect(shallowWrapper.find({ name: 'cc-name' }).length).toEqual(1);
+      expect(shallowWrapper.find({ name: 'cc-name' }).is(Field)).toEqual(true);
+    });
+  });
 });
