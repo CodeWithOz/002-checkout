abf705adb679d2117e58078f307de0a7312b142b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-16 18:24:17 +0100

    Test label of name Field

--- src/components/CardInfo/CardInfo.test.js
+++ src/components/CardInfo/CardInfo.test.js
@@ -9,11 +9,18 @@ describe('CardInfo renders', () => {
     expect(shallowWrapper.find('form').length).toEqual(1);
   });
 
-  describe('a Field with', () => {
+  describe('a name Field with', () => {
     test(`'name' set to 'cc-name'`, () => {
       const { shallowWrapper } = setup(CardInfo, props);
       expect(shallowWrapper.find({ name: 'cc-name' }).length).toEqual(1);
       expect(shallowWrapper.find({ name: 'cc-name' }).is(Field)).toEqual(true);
+    });
+
+    test(`'label' set to 'NAME ON CREDIT CARD'`, () => {
+      const { shallowWrapper } = setup(CardInfo, props);
+      expect(shallowWrapper.find({ name: 'cc-name' }).prop('label')).toEqual(
+        'NAME ON CREDIT CARD'
+      );
     });
   });
 });
