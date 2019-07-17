1051ca907ffdbc66a8587cbc8bf553733a2d847d
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-17 22:42:26 +0100

    Render a ValidationError in TextField

--- src/components/formFields/TextField/TextField.js
+++ src/components/formFields/TextField/TextField.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ValidationError } from '../../../components';
 
 export default function TextField({ label, meta, ...restProps }) {
   return (
     <div>
       <label htmlFor={restProps.id}>{label}</label>
       <input {...restProps} />
+      <p>
+        <ValidationError />
+      </p>
     </div>
   );
 }
