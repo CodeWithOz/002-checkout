8f7ea6b6957809efb8f8efd21571cb4905b81210
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-17 20:32:56 +0100

    Test if TextField passes 'type' prop
    Test already passes with current implementation.

--- src/components/formFields/TextField/TextField.test.js
+++ src/components/formFields/TextField/TextField.test.js
@@ -1,6 +1,6 @@
 import TextField from './TextField';
 
-const props = { id: 'testId', label: 'testLabel' };
+const props = { id: 'testId', label: 'testLabel', type: 'testType' };
 
 describe('TextField renders', () => {
   test('renders an input element', () => {
@@ -13,5 +13,12 @@ describe('TextField renders', () => {
     expect(shallowWrapper.find('label').length).toEqual(1);
     expect(shallowWrapper.find('label').prop('htmlFor')).toEqual(props.id);
     expect(shallowWrapper.find('label').text()).toContain(props.label);
+  });
+});
+
+describe('TextField correctly passes', () => {
+  test(`its 'type' prop to the input element`, () => {
+    const { shallowWrapper } = setup(TextField, props);
+    expect(shallowWrapper.find('input').prop('type')).toEqual(props.type);
   });
 });
