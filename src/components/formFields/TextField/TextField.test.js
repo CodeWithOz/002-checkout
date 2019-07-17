78d7ea3199bfb90c621a820fa65691e58cf47c78
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-17 20:18:08 +0100

    Test if TextField renders an associated label

--- src/components/formFields/TextField/TextField.test.js
+++ src/components/formFields/TextField/TextField.test.js
@@ -1,10 +1,17 @@
 import TextField from './TextField';
 
-const props = {};
+const props = { id: 'testId', label: 'testLabel' };
 
 describe('TextField renders', () => {
   test('renders an input element', () => {
     const { shallowWrapper } = setup(TextField, props);
     expect(shallowWrapper.find('input').length).toEqual(1);
+  });
+
+  test('renders a label for the input element', () => {
+    const { shallowWrapper } = setup(TextField, props);
+    expect(shallowWrapper.find('label').length).toEqual(1);
+    expect(shallowWrapper.find('label').prop('htmlFor')).toEqual(props.id);
+    expect(shallowWrapper.find('label').text()).toContain(props.label);
   });
 });
