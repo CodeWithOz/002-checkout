f7c530c7f0c42b93abfdbcd0cd50320e9b0bf5e0
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-18 07:17:56 +0100

    Create SelectDropdown component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,9 @@ export { default as BgImgContainer } from './BgImgContainer/BgImgContainer';
 export { default as ProductDisplay } from './ProductDisplay/ProductDisplay';
 export { default as SliderRange } from './formFields/SliderRange/SliderRange';
 export { default as TextField } from './formFields/TextField/TextField';
+export {
+  default as SelectDropdown
+} from './formFields/SelectDropdown/SelectDropdown';
 export { default as Slider } from './Slider/Slider';
 export { default as Payment } from './Payment/Payment';
 export { default as Breakdown } from './Breakdown/Breakdown';
