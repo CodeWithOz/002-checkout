652f26f6dceb41d79cca81a9d10b1393e35e8cc7
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-19 14:36:06 +0100

    Style TextField

--- src/components/formFields/TextField/TextField.js
+++ src/components/formFields/TextField/TextField.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { string, object } from 'prop-types';
 import { ValidationError } from '../../../components';
 
+import styles from './TextField.module.css';
+
 export default function TextField({ label, meta, input, ...restProps }) {
   return (
-    <div>
-      <label htmlFor={restProps.id}>{label}</label>
-      <input {...input} {...restProps} />
-      <p>
+    <div className={styles.root}>
+      <div>
+        <label className={styles.label} htmlFor={restProps.id}>
+          {label}
+        </label>
+      </div>
+      <div>
+        <input className={styles.input} {...input} {...restProps} />
+      </div>
+      <div>
         <ValidationError meta={meta} />
-      </p>
+      </div>
     </div>
   );
 }
