bd9c7137c1cf279da0e34bfdb7c7a0e82a468368
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-19 08:59:22 +0100

    Remove margin on body tag

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BgImgContainer, ProductDisplay, Payment } from '../../components';
 
+import './App.css';
+
 const appConfig = {
   bgImgUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg'
 };
