6b01c28a580f31368ea09386686fe77029710281
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-20 16:46:07 +0100

    Change the layout on larger viewports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BgImgContainer, ProductDisplay, Payment } from '../../components';
 
 import './App.css';
+import styles from './App.module.css';
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.root}>
       <BgImgContainer>
-        <ProductDisplay />
-        <Payment />
+        <div className={styles.content}>
+          <ProductDisplay />
+          <Payment />
+        </div>
       </BgImgContainer>
     </div>
   );
