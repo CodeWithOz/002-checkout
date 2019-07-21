88d43390c9236070a032baabe3e4aea4988ba0f0
Uche Ozoemena <ucheozoemena@gmail.com>
2019-07-21 06:45:19 +0100

    Change the name of a class

--- src/components/BgImgContainer/BgImgContainer.js
+++ src/components/BgImgContainer/BgImgContainer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './BgImgContainer.module.css';
 
 export default function BgImgContainer({ bgImgUrl, children }) {
-  return <div className={styles.BgImgContainer}>{children}</div>;
+  return <div className={styles.root}>{children}</div>;
 }
